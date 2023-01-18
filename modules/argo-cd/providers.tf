a5629a7afe797250c0aa4474ba905432237a0e2c
ccastro670 <60413973+ccastro670@users.noreply.github.com>
2023-01-18 16:27:56 -0400

    feat : adding kubernetes provider.

--- modules/argo-cd/providers.tf
+++ modules/argo-cd/providers.tf
@@ -7,5 +7,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "2.8.0"
     }
+    
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.16.1"
+    }
   }  
 }
