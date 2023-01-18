721433ff190626f8df8af9742dc6ccca30c1a8da
ccastro670 <60413973+ccastro670@users.noreply.github.com>
2023-01-18 16:36:02 -0400

    feat: (argued) adding secrets file.

---
+++ modules/argo-cd/secrets.tf
@@ -0,0 +1,16 @@
+resource "kubernetes_secret" "github_oauth" {
+  metadata {
+    name = var.github_secret_name
+    namespace = var.argocd_namespace
+    labels = {
+      "app.kubernetes.io/part-of" = "argocd"
+    }
+  }
+
+  data = {
+    github.client.id = var.github_oauth_client_id
+    github.client.secret = var.github_oauth_client_secret
+  }
+
+  type = "Opaque"
+}
