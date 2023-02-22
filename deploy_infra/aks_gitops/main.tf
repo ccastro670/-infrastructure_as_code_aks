5678a08749b28ffe1e97a9888a20417717c4a52a
ccastro670 <60413973+ccastro670@users.noreply.github.com>
2023-02-21 23:30:47 -0400

    Update main.tf

--- deploy_infra/aks_gitops/main.tf
+++ deploy_infra/aks_gitops/main.tf
@@ -53,6 +53,8 @@ module "aks_cluster" {
   aks_mode_nodepool_user     = var.aks_mode_nodepool_user
   aks_agent_os_disk_size     = var.aks_agent_os_disk_size 
   kube_config_path           = var.kube_config_path 
+  gateway_id                 = var.gateway_id
+  subnet_id                  = var.appgw_subnet_id
   tags                       = var.tags
 }
 
